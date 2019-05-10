4a8cdc86f89de3741bf8c1406bf4c950bd8b23d3
Kamber L Borders <kamber.borders@gmail.com>
2019-05-10 15:29:15 -0700

    Update devbuild.sh

--- devbuild.sh
+++ devbuild.sh
@@ -211,7 +211,17 @@ else
 fi
 sleep 1
 
-# Set up darkmode for Slack
-echo "FIX ME"
-
-#
+# Set up darkmode for 
+DARKMODE="
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  let tt__customCss = '.menu ul li a:not(.inline_menu_link) {color: #fff !important;}'
+  $.ajax({
+       url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+      success: function(css) {
+          $('<style></style>').appendTo('head').html(css + tt__customCss);
+      }
+ });
+});
+"
+echo "$DARKMODE" >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
